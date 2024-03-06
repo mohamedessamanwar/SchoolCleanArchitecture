04dd915af3bb1eb502fb245c83fa33e9b3366ffb
mohamedessamanwar <103913494+mohamedessamanwar@users.noreply.github.com>
2024-03-06 06:37:33 +0200

    Update StudentController.cs

--- SchoolCleanArchitecture.Api/Controllers/StudentController.cs
+++ SchoolCleanArchitecture.Api/Controllers/StudentController.cs
@@ -27,7 +27,7 @@ namespace SchoolCleanArchitecture.Api.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult> GetStudent(int id)
         {
-        //
+        //v
             var response = await _mediator.Send(new GetStudentRequst { Id=id});
             return Ok(response);
 
